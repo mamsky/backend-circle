a3304c1c8f9f17da49cec01e462a4b2cab49cef6
mamsky <pasteprosmana189@gmail.com>
2025-02-14 02:51:22 +0700

    add husky prettier lint-staged

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
-import express, { Request, Response } from "express";
-import dotenv from "dotenv";
+import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hallo World");
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hallo World');
 });
 
 app.listen(port, () => {
